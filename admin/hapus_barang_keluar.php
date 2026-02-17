10346633a628b66ee568d2196c648fb108e0b11f
auliari <aulia.arifadhilah20@gmail.com>
2026-02-17 08:37:18 +0700

    Delete admin directory

--- admin/hapus_barang_keluar.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include '../koneksi.php';
-    $id = $_GET['bk_id'];
-    mysqli_query($koneksi, "delete from barang_keluar where bk_id = '$id'");
-
-    echo "<script>alert('Data berhasil dihapus.');window.location='barang_keluar.php';</script>";
-?>
--- admin/hapus_barang_masuk.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include '../koneksi.php';
-    $id = $_GET['bm_id'];
-    mysqli_query($koneksi, "delete from barang_masuk where bm_id = '$id'");
-
-    echo "<script>alert('Data berhasil dihapus.');window.location='barang_masuk.php';</script>";
-?>
